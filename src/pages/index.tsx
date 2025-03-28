b18c960dcb076adbdf308205041baa5c21e3ad05
tison <wander4096@gmail.com>
2025-03-28 11:14:41 +0800

    feat: add simple homepage for crawling index
    Signed-off-by: tison <wander4096@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
-import { Redirect } from "@docusaurus/router";
-import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from './index.module.css';
+import Layout from "@theme/Layout";
+import clsx from "clsx";
 import React from "react";
 
+function HomepageBanner(): React.JSX.Element {
+  return (
+    <div className={styles.scopeBanner}>
+      <div className={styles.scopeBannerContent}>
+        <h1 className={styles.scopeBannerTitle}>
+          ScopeDB Documents
+        </h1>
+        <div className={styles.scopeBannerIntro}>
+          Streamline your data management with ScopeDB's comprehensive documentation.
+        </div>
+        <div className={styles.scopeBtns}>
+          <a href="/reference/overview" className={clsx(styles.scopeBtn, styles.scopeBtnPrimary)}>Read the Docs</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Home(): React.JSX.Element {
-  return <Redirect to={useBaseUrl("reference/overview")} />
+  return (
+    <Layout>
+      <HomepageBanner />
+    </Layout>
+  );
 }
