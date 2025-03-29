93136de58bfedbeb28343fb79075229068f87867
tison <wander4096@gmail.com>
2025-03-29 18:52:48 +0800

    feat: initialize
    Signed-off-by: tison <wander4096@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import mdx from '@astrojs/mdx';
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [mdx()],
+    outDir: './build',
+});
